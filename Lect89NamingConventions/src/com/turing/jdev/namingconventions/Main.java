fb5e63056b9f9fa5d7f45b2f58d715b64531a216
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-18 01:27:28 +0200

    Nice challenge to test scope. Challenge is in the README.

--- Lect89NamingConventions/src/com/turing/jdev/namingconventions/Main.java
+++ Lect89NamingConventions/src/com/turing/jdev/namingconventions/Main.java
@@ -1,5 +1,7 @@
 package com.turing.jdev.namingconventions;
 
+import java.util.Scanner;
+
 public class Main {
 	
 	public static void main(String[] args){
@@ -7,7 +9,7 @@ public class Main {
 		/*MyWindow myWindow = new MyWindow("Java dev");
 		myWindow.setVisible(true);*/
 		
-		int n = 10;
+		/*int n = 10;
 		System.out.println("Sum of " + n + " : " + Series.nSum(n));
 		
 		for(int i = 1; i <= n; i++){
@@ -16,7 +18,31 @@ public class Main {
 		
 		for(int i = 1; i <= n; i++){
 			System.out.println("Fibonacci of " + n + " : " + Series.Fibonacci(i));
-		}
+		}*/
+		
+		// Write a small program to read an integer from the keyboard
+        // (using Scanner) and print out the times table for that number.
+        // The table should run from 1 to 12.
+        //
+        // You are allowed to use one variable called scanner for your
+        // Scanner instance. You can use as many other variables as you
+        // need, but they must must all be called x. That includes any
+        // class instances and loop control variables that you may decide
+        // to use.
+        //
+        // If you use a class, the class can be called X (capital), but
+        // any instances of it must be called x (lower case).
+        //
+        // Any methods you create must also be called x.
+        //
+        // Optional Extra:
+        // Change your program so that ALL variables (including the scanner
+        // instance) are called x.
+		
+		X x = new X(new Scanner(System.in));
+		x.x();
+		
+		
 		
 	}
 }
