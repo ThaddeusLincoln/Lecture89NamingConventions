7301b05a8e3d898e1bc3f9a899e48a6bf7077d84
ThaddeusLincoln <beny1419@yahoo.es>
2016-08-18 01:01:36 +0200

    Now implemented some basic Mathematical Functions like fibonacci.

--- Lect89NamingConventions/src/com/turing/jdev/namingconventions/Main.java
+++ Lect89NamingConventions/src/com/turing/jdev/namingconventions/Main.java
@@ -4,8 +4,16 @@ public class Main {
 	
 	public static void main(String[] args){
 		
-		MyWindow myWindow = new MyWindow("Java dev");
-		myWindow.setVisible(true);
+		/*MyWindow myWindow = new MyWindow("Java dev");
+		myWindow.setVisible(true);*/
+		
+		int n = 10;
+		System.out.println("Factorial of " + n + " : " + Series.factorial(n));
+		System.out.println("Factorial of " + n + " : " + Series.nSum(n));
+		
+		for(int i = 1; i <= n; i++){
+			System.out.println("Factorial of " + n + " : " + Series.Fibonacci(i));
+		}
 		
 	}
 }
